ab2029b518a0e174d64f8d047ed54156c023b20f
Zach  Smith <zdsmith001@gmail.com>
2019-08-22 20:45:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   attr_accessor :total, :discount
-  items = []
+  @items = []
 
   def initialize(discount = 0)
     @total = 0
@@ -24,7 +24,7 @@ class CashRegister
   end
 
   def items
-    items
+    @items
   end
 
 end
