a5e7d411134746e0180f2edc7df12d5d8d3ce723
Zach  Smith <zdsmith001@gmail.com>
2019-08-22 21:09:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     quantity.times do
       @items << title
     end
-    @last_transaction = price * quantity
+    self.last_transaction = price * quantity
   end
 
   def apply_discount
