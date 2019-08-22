1818801f176ff0b0fa11fe1616cb575bbab4adf9
Zach  Smith <zdsmith001@gmail.com>
2019-08-22 20:49:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     price = price * quantity
     @total += price
-    @items << title * quantity
+    @items << title
   end
 
   def apply_discount
